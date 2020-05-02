4c1445f1fc822f76bb436e42f877a2ff064985eb
Saulo Arantes <saulovinicius987@gmail.com>
2020-05-01 23:45:44 -0300

    Adding references on home footer

--- lib/widgets/home_page_body.dart
+++ lib/widgets/home_page_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:planets/models/Planet.dart';
 import 'package:planets/utils/styles.dart';
 import 'package:planets/widgets/planet_row.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePageBody extends StatelessWidget {
   @override
@@ -25,11 +26,42 @@ class HomePageBody extends StatelessWidget {
             SliverToBoxAdapter(
               child: Container(
                 padding: EdgeInsets.only(bottom: 10.0),
-                child: Text(
-                  'Icons made by Smashicons from www.flaticon.com \n Developed by Saulo Arantes',
-                  style: Styles.commonTextStyle,
-                  textAlign: TextAlign.center,
-                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: () async {
+                        const url = 'https://www.flaticon.com/authors/smashicons';
+                        if (await canLaunch(url)) {
+                          await launch(url);
+                        } else {
+                          throw 'Could not launch $url';
+                        }
+                      },
+                      child: Text(
+                        'Icons made by Smashicons from www.flaticon.com',
+                        style: Styles.commonTextStyle,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+
+                    GestureDetector(
+                      onTap: () async {
+                        const url = 'https://github.com/saulo-arantes/planets';
+                        if (await canLaunch(url)) {
+                          await launch(url);
+                        } else {
+                          throw 'Could not launch $url';
+                        }
+                      },
+                      child: Text(
+                        'Developed by Saulo Arantes',
+                        style: Styles.commonTextStyle,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                )
               )
             )
           ],
