f5d7aeeedc5fccda9853f83155d0237312ba3768
Saulo Arantes <saulovinicius987@gmail.com>
2020-05-01 22:56:06 -0300

    Adding launch icons and changing app name

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Planets',
+      title: 'Sistema Solar',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
